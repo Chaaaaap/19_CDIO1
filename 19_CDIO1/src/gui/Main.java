52d5dc0853e5de325be57c81669f522fc924d56f
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2016-02-08 15:03:04 +0100

    Leger videre med GUI

---
+++ 19_CDIO1/src/gui/Main.java
@@ -0,0 +1,17 @@
+package gui;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		
+		new ();
+		
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//			public void run() {
+//				createAndShowGUI();
+//			}
+//		});
+	}
+}
