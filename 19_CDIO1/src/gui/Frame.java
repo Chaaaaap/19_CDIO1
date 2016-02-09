51db06d1e64ac71be013dc14bcee3a9d8d55fcaa
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2016-02-09 15:30:22 +0100

    Gui begynder at ligne noget :D

--- 19_CDIO1/src/gui/Frame.java
+++ 19_CDIO1/src/gui/Frame.java
@@ -18,81 +18,63 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class Frame implements ActionListener {
+public class Frame {
 
 	
-	public static void main(String[] args) {
-		//Schedule a job for the event-dispatching thread:
-		//creating and showing this application's GUI.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createAndShowGUI();
-			}
-		});
-	}
-	
-	public static void addComponentsToPane(Container pane) {
+	public void addComponentsToPane(Container pane) {
 		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 		
-		addLabel("Afvejningssystem", pane);
-		addAButton(pane);
+		addLabel("Afvejningssystem", pane, 25.0f);
+		addAButton(pane, "Login", new LoginButton());
+		addAButton(pane, "Sign Up", new SignUpButton());
 
 
 	}
 
-	public static void addLabel(String text, Container container) {
-		JLabel field = new JLabel(text);
+	public void addLabel(String text, Container container, Float fontSize) {
+		JLabel label = new JLabel(text);
+		label.setAlignmentX(Component.CENTER_ALIGNMENT);
+		label.setFont(label.getFont().deriveFont(fontSize));
+		container.add(label);
+		container.add(Box.createVerticalGlue());
+
+	}
+
+
+	public void addAButton(Container container, String btnName, ActionListener event) {
+
+
+		JButton button = new JButton(btnName);
+		button.addActionListener(event);
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button.setMaximumSize(new Dimension(100, 30));
+		container.add(button);
+		container.add(Box.createVerticalGlue());
+
+	}
+	
+	public void addTextField(Container container) {
+		JTextField field = new JTextField();
 		field.setAlignmentX(Component.CENTER_ALIGNMENT);
-		field.setFont(field.getFont().deriveFont(25.0f));
+		field.setMaximumSize(new Dimension(200,30));
 		container.add(field);
 		container.add(Box.createVerticalGlue());
-
 	}
 
 
-	public static void addAButton(Container container) {
-
-
-		JButton loginBtn = new JButton("Login");
-		loginBtn.addActionListener(new LoginButton());
-		loginBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		loginBtn.setMaximumSize(new Dimension(100, 30));
-		container.add(loginBtn);
-		container.add(Box.createVerticalGlue());
-
-		JButton signUpBtn = new JButton("Sign Up");
-		signUpBtn.addActionListener(new SignUpButton());
-		signUpBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-		signUpBtn.setMaximumSize(new Dimension(100, 30));
-		container.add(signUpBtn);
-		container.add(Box.createVerticalGlue());
-
-
-	}
-
-	/**
-	 * Create the GUI and show it.  For thread safety,
-	 * this method should be invoked from the
-	 * event-dispatching thread.
-	 */
-	public static void createAndShowGUI() {
+	public void createAndShowGUI(String title) {
 		//Create and set up the window.
-		JFrame frame = new JFrame("Main Menu");
+		JFrame frame = new JFrame(title);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//Set up the content pane.
 		addComponentsToPane(frame.getContentPane());
 
 		//Display the window.
+		
 		frame.pack();
 		frame.setVisible(true);
 		frame.setSize(600, 180);
 		frame.setLocationRelativeTo(null);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 }
--- 19_CDIO1/src/gui/SignUpButton.java
+++ 19_CDIO1/src/gui/SignUpButton.java
@@ -1,14 +1,46 @@
 package gui;
 
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+
 public class SignUpButton implements ActionListener {
 
+	Frame f = new Frame();
+	
+	private void addComponentsToPane(Container contentPane) {
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		f.addLabel("Sign Up Menu", contentPane , 25.0f);
+		f.addLabel("Username", contentPane, 15.0f);
+		f.addTextField(contentPane);
+		f.addLabel("Password", contentPane, 15.0f);
+		f.addTextField(contentPane);
+		
+	}
+	
+	public void createAndShowGUI() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("Sign Up page");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Set up the content pane.
+		addComponentsToPane(frame.getContentPane());
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(600, 250);
+		frame.setLocationRelativeTo(null);
+	}
+	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
+		createAndShowGUI();
 		System.out.println("Sign Up");
 
 	}
-
 }
