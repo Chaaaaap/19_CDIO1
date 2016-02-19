86c666592f4c996d187c09a7f876e928d38c4ead
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-02-19 13:32:07 +0100

    .

--- 19_CDIO1/src/tui/TUIRun.java
+++ 19_CDIO1/src/tui/TUIRun.java
@@ -13,7 +13,7 @@ public class TUIRun {
 	private OperatorDTO odto;
 	private int oprId;
 	private String password;
-	
+
 	public TUIRun() {
 		mainMenu(scan);
 		odto = new OperatorDTO();
@@ -21,7 +21,7 @@ public class TUIRun {
 
 	public void mainMenu(Scanner scan){
 		System.out.println("Velkommen til 'applikation-navn'!\n");
-		
+
 		do{
 			System.out.println("Du har nu følgende valgmuligheder:\n"
 					+ "1. Opret ny bruger.\n"
@@ -67,7 +67,7 @@ public class TUIRun {
 			if(choice == 1){
 				if(loginSysAdmin()) {
 					cont.createrOperator();
-					
+
 				}
 			}
 			else if(choice == 2){
@@ -107,27 +107,27 @@ public class TUIRun {
 		String newpass2 = "";
 		String newpass = " ";
 		do {
-		System.out.println("Indtast bruger nr.");
-		int oprId = scan.nextInt();
-		System.out.println("Indtast din gamle adgangskode");
-		String password = scan.nextLine();
-		if(passwordCheck(oprId, password)) {
-		System.out.println("Indtast din nye adgangskode");
-		newpass = scan.nextLine();
-		System.out.println("Gentag din nye adgangskode");
-		newpass2 = scan.nextLine();
-		if(newpass.equals(newpass2))
-			if(odto.changePassword(newpass));
-				
-			else
-				System.out.println("Der skal være minimum et stort bogstav, et lille bogstav og et tal i din kode. "
-						+ "Desuden skal den være minimum 6 karakterer lang.");
-		else
-			System.out.println("Koderne er ikke ens. Prøv igen.");
-		} else {
-			System.out.println("Bruger nr og password passer ikke sammen.");
-		}
-	}while(!newpass.equals(newpass2));
+			System.out.println("Indtast bruger nr.");
+			int oprId = scan.nextInt();
+			System.out.println("Indtast din gamle adgangskode");
+			String password = scan.nextLine();
+			if(passwordCheck(oprId, password)) {
+				System.out.println("Indtast din nye adgangskode");
+				newpass = scan.nextLine();
+				System.out.println("Gentag din nye adgangskode");
+				newpass2 = scan.nextLine();
+				if(newpass.equals(newpass2))
+					if(odto.changePassword(newpass));
+
+					else
+						System.out.println("Der skal være minimum et stort bogstav, et lille bogstav og et tal i din kode. "
+								+ "Desuden skal den være minimum 6 karakterer lang.");
+				else
+					System.out.println("Koderne er ikke ens. Prøv igen.");
+			} else {
+				System.out.println("Bruger nr og password passer ikke sammen.");
+			}
+		}while(!newpass.equals(newpass2));
 	}
 
 	public void balancing(Scanner scan){
@@ -152,41 +152,41 @@ public class TUIRun {
 		}
 		while(choice > 2);
 	}
-	
+
 	public boolean oprLogin(Scanner scan) {
 		String password;
 		int oprId;
-		
+
 		System.out.println("Bruger nr.:");
 		oprId = scan.nextInt();
 		System.out.println("Password:");
 		password = scan.next();
-		
+
 		return passwordCheck(oprId, password);
-		
+
 	}
-	
+
 	public boolean passwordCheck(int oprId, String password) {
 		for(int i = 0; i < odto.getOperatorList().size(); i++)
 			if(odto.getOperatorList().get(i).getOprId() == oprId)
 				if(odto.getOperatorList().get(i).getPassword(oprId).equals(password))
 					return true;
-		
+
 		return false;
 	}
-	
+
 	public double afvejning(Scanner scan) {
-		
+
 		System.out.println("Afvejning.\n");
 		System.out.println("Indtast emballage vægt i kg.");
 		double emballage = scan.nextDouble();
 		System.out.println("Indtast bruttovægt i kg.");
 		double brutto = scan.nextDouble();
-		
+
 		double netto = brutto - emballage;
-		
+
 		return netto;
-		
+
 	}
 
 	public boolean loginSysAdmin(){
@@ -199,9 +199,9 @@ public class TUIRun {
 		oprId = scan.nextInt();
 		System.out.println("Password:");
 		password = scan.next();
-		
+
 		scan.close();
-		
+
 		if(sysAdminOprId() == oprId && password.equals(sysAdminPassword())){
 			return true;
 		}
@@ -209,9 +209,9 @@ public class TUIRun {
 			System.out.println("Forkert bruger nr. eller password. Du bliver ført tilbage til foregående menu. \n");
 			createUser(scan);
 			return false;
-			
+
 		}
-		
+
 	}
 
 	public int sysAdminOprId(){
